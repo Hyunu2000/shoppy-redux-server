541a738cf04cda91a54de31c757f0e73d95cf063
kang <rkdgusdn0319@gmail.com>
2025-04-08 17:05:27 +0900

    commit

--- repository/db.js
+++ repository/db.js
@@ -4,7 +4,7 @@
 import mysql from 'mysql2';
 
 const pool = mysql.createPool({
-    host: 'shoppy-redux-db.cfcsuo2oi4f7.ap-northeast-2.rds.amazonaws.com',
+    host: 'hyunu2000db.cfcsuo2oi4f7.ap-northeast-2.rds.amazonaws.com',
     port: 3306,
     user: 'admin',
     password: 'aArkdgusdn2',
